3e5d96e173445421bb5d8abb55ff61163eb23433
Alex Ginsberg <alex.ginsberg@richmond.edu>
2017-09-28 14:22:12 -0400

    Creates a user upon signup; need to add that user to the user model

--- client/components/index.js
+++ client/components/index.js
@@ -7,5 +7,6 @@
 export {default as Main} from './main'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
+export {default as UserSignup} from './userSignup'
 
 
--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome} from './components'
+import {Main, Login, Signup, UserHome, UserSignup} from './components'
 import {me} from './store'
 
 /**
@@ -24,7 +24,7 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are available to all visitors */}
             <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
+            <Route path='/signup' component={UserSignup} />
             {
               isLoggedIn &&
                 <Switch>
