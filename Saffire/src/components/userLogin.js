6bcc4487dde3790472f81a52c2a5348ed69f74e0
Luke Zhang <Lukezhang10@gmail.com>
2017-09-29 14:41:08 -0400

    edited for warnings - unused variables

--- Saffire/src/components/userLogin.js
+++ Saffire/src/components/userLogin.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import firebase from '../firebase'
 
 /**
--- Saffire/src/store/index.js
+++ Saffire/src/store/index.js
@@ -7,7 +7,6 @@ import currentItinerary from '../reducers/reducer_WhereTo'
 
 
 const reducer = combineReducers({user, currentItinerary})
-const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 const store = createStore(reducer, applyMiddleware(thunkMiddleware, createLogger()));
 
 export default store
