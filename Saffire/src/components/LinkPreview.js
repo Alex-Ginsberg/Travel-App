75c98de4a4c0660db3cd074c9ec303b9fc1c34c4
Alex Ginsberg <alex.ginsberg@richmond.edu>
2017-09-30 14:28:32 -0400

    Styling fixes to LinkPreview

--- Saffire/src/components/LinkPreview.js
+++ Saffire/src/components/LinkPreview.js
@@ -46,7 +46,11 @@ class LinkPreview extends Component {
       likedByArray.push(this.props.likedBy[key].name)
     }
     return(
-      <Card expanded={false}>
+      <Card>
+        <CardHeader
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
         <CardMedia
           overlay={<CardTitle title={this.props.title} />}
           mediaStyle={{width: 300, height: 300}}
@@ -58,6 +62,10 @@ class LinkPreview extends Component {
           <FlatButton label={`Like ${this.props.likes}`} onClick={() => this.props.newLike(this.props.eventKey, this.props.itinKey)} disabled={likedByArray.includes(firebase.auth().currentUser.email)}/>
           {!this.props.hasBeenAdded && <FlatButton label="Add To Itinerary" onClick={() => this.props.confirmEvent(this.props.eventKey, this.props.itinKey) }/>}
         </CardActions>
+        <CardText expandable={true}>
+          Liked by: {likedByArray} <br></br>
+          {this.props.description}
+        </CardText>
       </Card>
     )
   }
