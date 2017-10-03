fa473ccee502c0813f0d02cdefe5d0fa0bf4b203
Luke Zhang <Lukezhang10@gmail.com>
2017-10-03 15:40:55 -0400

    can push and receive notifcations

--- Saffire/src/firebase.js
+++ Saffire/src/firebase.js
@@ -29,14 +29,94 @@ messaging.requestPermission()
     console.log('have permission');
     return messaging.getToken()
   })
-  .then(token => console.log('token', token))
+  .then(token => {
+    console.log('token', token)
+    sendTokenToServer(token)
+  })
   .catch((err) => {
     console.log('no permission')
     console.log(err)
   });
 
 
+messaging.onMessage(function(payload) {
+  console.log('onMessage firebase.js', payload);
+})
 
+
+
+function sendTokenToServer(currentToken) {
+  if (!isTokenSentToServer()) {
+    console.log('Sending token to server...');
+    // TODO(developer): Send the current token to your server.
+    setTokenSentToServer(true);
+  } else {
+    console.log('Token already sent to server so won\'t send it again ' +
+        'unless it changes');
+  }
+}
+
+function isTokenSentToServer() {
+  return window.localStorage.getItem('sentToServer') == 1;
+}
+
+function setTokenSentToServer(sent) {
+  window.localStorage.setItem('sentToServer', sent ? 1 : 0);
+}
+
+
+  // [START refresh_token]
+  // Callback fired if Instance ID token is updated.
+  messaging.onTokenRefresh(function() {
+    messaging.getToken()
+    .then(function(refreshedToken) {
+      console.log('Token refreshed.');
+      // Indicate that the new Instance ID token has not yet been sent to the
+      // app server.
+      setTokenSentToServer(false);
+      // Send Instance ID token to app server.
+      sendTokenToServer(refreshedToken);
+      // [START_EXCLUDE]
+      // Display new Instance ID token and clear UI of all previous messages.
+      resetUI();
+      // [END_EXCLUDE]
+    })
+    .catch(function(err) {
+      console.log('Unable to retrieve refreshed token ', err);
+    });
+  });
+  // [END refresh_token]
+
+
+
+  function resetUI() {
   
+    // [START get_token]
+    // Get Instance ID token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    messaging.getToken()
+    .then(function(currentToken) {
+      if (currentToken) {
+        sendTokenToServer(currentToken);
+
+      } else {
+        // Show permission request.
+        console.log('No Instance ID token available. Request permission to generate one.');
+        // Show permission UI.
+        setTokenSentToServer(false);
+      }
+    })
+    .catch(function(err) {
+      console.log('An error occurred while retrieving token. ', err);
+      setTokenSentToServer(false);
+    });
+  }
+  // [END get_token]
+
+
+
+   
+
+
 
 export default firebase
