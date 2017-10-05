5a1c9bbfecefd1f9f2405c4affd9023f1e85c381
Jason Ko <JasonKo@Jasons-MacBook-Pro.local>
2017-10-05 12:30:22 -0400

    new-stuff

--- Saffire/src/components/AllItineraries.js
+++ Saffire/src/components/AllItineraries.js
@@ -28,6 +28,7 @@ class AllItineraries extends React.Component {
   }
 
   render () {
+ 
     let toRenderItins = []
     if (this.props.currentUser.key) {
       for (var i = 0; i < this.state.itinArray.length; i++) {
@@ -43,7 +44,9 @@ class AllItineraries extends React.Component {
         }
       }
     }
+    
 
+    console.log(this.props.currentUser, "(((((")
     return (
       <div className="saffire-all-itineraries-div">
         
@@ -65,7 +68,7 @@ class AllItineraries extends React.Component {
                 
                 <span className = "sapphire-itin-name-x">{itin.name}</span>
                 <span className = "sapphire-itin-name-y">10 Aug 2017 - 15 Aug 2017</span>
-                <span className = "sapphire-itin-name-z"> <span className="glyphicon glyphicon-user"></span> 8 Friends</span>
+                <span className = "sapphire-itin-name-z"> <span className="glyphicon glyphicon-user"></span>{this.props.currentUser.friends !== undefined ? Object.keys(this.props.currentUser.friends).length + " Friends" : "0 Friends"}</span>
                 
                 </div>
             </div>
