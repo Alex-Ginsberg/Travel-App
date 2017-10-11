0922cdfa700c69d7f61afd6f6b8a725289ef7043
Jason Ko <JasonKo@Jasons-MacBook-Pro.local>
2017-10-11 15:29:33 -0400

    styleimproved

--- Saffire/src/components/myFriends.js
+++ Saffire/src/components/myFriends.js
@@ -25,70 +25,45 @@ class MyFriends extends React.Component {
         Object.keys(this.props.user.notifications).map(currentKey => {
             notificationArray.push(this.props.user.notifications[currentKey])
         })
-        console.log('NOTIFICATIONS: ', notificationArray)
     }
     return (
       <div className="saffire-friends-container">
-          <div className="friends-header">
-            <BurgerMenu />
-              <h1>SAFFIRE</h1>
-          </div>
+        
+        <BurgerMenu />
+        <h1>SAFFIRE</h1>
 
-
-          <div className="row">
-
-              <div className="col-lg-8">
-                  <div className="my-friends-list">
-                  <h1 className = "saffire-friends-title">My Friends</h1>
-                    <div className = "saffire-friends-little-wrapper">
-                        {
-                         friends.map(friend => {
-                             return (
-                                 <div className = "saffire-friend-item">
-                                     <img src = {toonAvatar.generate_avatar()} />
-                                        <div className = "friend-align">
-                                            <p>{this.props.currentUser[friend].name}</p>
-                                            <p>{this.props.currentUser[friend].email} </p>
-                                        </div>
+        <div className="saffire-friends-column-8 clearfix">
+            <h2>MY FRIENDS</h2>
+            <div className = "saffire-friends-little-wrapper">
+                {
+                    friends.map(friend => {
+                        return (
+                            <div className = "saffire-friend-item">
+                                <img src = {toonAvatar.generate_avatar()} />
+                                <div className = "friend-align">
+                                    <p>{this.props.currentUser[friend].name}</p>
+                                    <p>{this.props.currentUser[friend].email} </p>
                                 </div>
-                             )
-                         })
-                        }
-                    </div>
-                  </div>
-              </div>
+                        </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
 
 
-              <div className="col-lg-3">
-                  <div className="saffire-friends-dash-friendUtilities">
-
-                      <div className="find-friend">
-                      <FindFriends />
-                      </div>
-                            {notificationArray.length > 0 && 
-                            <div>
-                                <h3>Your notifications:</h3>
-                                {notificationArray.map(notification => (
-                                    <h5 onClick={() => this.props.removeNotification(this.props.user, notification.body)}>{notification.body}</h5>
-                                ))}
-                            </div>
-                            }
-
-                      <div className="friend-requests">
-                      <FriendRequests />
-                      </div>
-
-                  </div>
-
-
-
-              </div>
-
-
-
-
-          </div>
-
+        <div className="sapphire-friends-column-4 clearfix">
+            <FindFriends />
+            { notificationArray.length > 0 && 
+                <div className = "sapphire-friends-notification-div">
+                    <h3>MY NOTIFICATIONS</h3>
+                    {notificationArray.map(notification => (
+                        <p onClick={() => this.props.removeNotification(this.props.user, notification.body)}>{notification.body}</p>
+                    ))}
+                </div>
+            }
+            <FriendRequests />
+        </div>
       </div>
     )
   }
